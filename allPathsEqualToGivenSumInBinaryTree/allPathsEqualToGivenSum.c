56bed4d07be1bb575a853177e5f527a2a0f9f883
pankaj <pankaj@pankaj-Inspiron-N4010.(none)>
2015-09-13 13:37:01 +0530

    added program to find all possible paths equal to given sum in binary tree

---
+++ allPathsEqualToGivenSumInBinaryTree/allPathsEqualToGivenSum.c
@@ -0,0 +1,28 @@
+	/* its only the c function which prints all paths in a binary tree that sums up to given sum. 
+           The code is designed to work for negative numbers as well.
+ 	   Time Complextiy := o(n)  */
+
+	/* for this function to work, put this in any binary tree implementation code and pass the initial parameters as follows
+
+	root = root of tree;
+	index = 0
+	sum = 0
+	givenSum = givenSum in problem */
+
+void findPaths{tree *root, int arr[], int index, int sum, int givenSum{
+
+	if(root == NULL)
+		return;
+	arr[index] = root -> data;
+	sum = sum + root -> data;
+	if(sum == givenSum){
+	
+		for(int i = 0; i <= index; i++){
+		
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+	}
+	findPaths(root -> left, arr, index + 1, sum, givenSum);
+	findPaths(root -> right, arr, index + 1, sum, givenSum);
+}
